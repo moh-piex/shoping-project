f6345bf929ab1270eda57f221b0823a52fe86d18
mohamed <maba80727@gmail.com>
2021-06-06 13:40:36 +0200

    the second project

---
+++ script.js
@@ -0,0 +1,34 @@
+/*var pic = document.getElementsByClassName("change");
+var i;
+window.onscroll = function na() {"use strict";
+if(pic[0,1,2,3].style.visibility == "hidden" & pic[0,1,2,3].style.opacity == "0") {
+    pic[0].style.visibility = "visible" ,  pic[0].style.opacity ="1" , pic[0].style.transition ="1s";
+}else if ( pic[0].style.visibility == "visible" &  pic[0].style.opacity =="1") {
+    pic[0].style.visibility = "hidden" ,  pic[0].style.opacity ="0" , pic[0].style.transition ="1s" ,
+    pic[1].style.visibility = "visible" ,  pic[1].style.opacity ="1";
+}else if (pic[1].style.visibility == "visible" &  pic[1].style.opacity =="1") {
+    pic[1].style.visibility = "hidden" ,  pic[1].style.opacity ="0" ,pic[0].style.transition ="1s" ,
+    pic[2].style.visibility = "visible" ,  pic[2].style.opacity ="1";
+} else if(pic[2].style.visibility == "visible" &  pic[2].style.opacity =="1") {
+    pic[2].style.visibility = "hidden" ,  pic[2].style.opacity ="0" ,pic[0].style.transition ="1s" ,
+    pic[3].style.visibility = "visible" ,  pic[3].style.opacity ="1";
+} else if (pic[3].style.visibility == "visible" &  pic[3].style.opacity =="1") {
+    pic[2].style.visibility = "hidden" ,  pic[2].style.opacity ="0" ,pic[0].style.transition ="1s" ,
+    pic[3].style.visibility = "visible" ,  pic[3].style.opacity ="1";
+}
+    
+
+}*/
+/*
+console.log(pic.length);
+window.onscroll = function na() {"use strict";
+if (pic[0,1,2,3].style.visibility == "hidden" & pic[0,1,2,3].style.opacity == "0") {
+    pic[0].style.visibility = "visible" ,  pic[0].style.opacity ="1" ;} else {
+        pic[1].style.visibility = "visible" ,  pic[1].style.opacity ="1";
+    }}*/
+
+console.dir(document)
+console.log(document.head)
+console.log(document.body)
+console.log(document.domain)
+console.log(document.all)
